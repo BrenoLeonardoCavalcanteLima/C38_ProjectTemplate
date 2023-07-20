6fb190f920abf392a91e1714eb97c7aba38399e0
BrenoLeonardoCavalcanteLima <paulo.victor.cavalcante.costa>
2023-07-20 15:51:30 -0300

    Projeto 38

--- js/Contestant.js
+++ js/Contestant.js
@@ -9,35 +9,23 @@ class Contestant {
     var contestantCountRef = database.ref('contestantCount');
 
 
-    //contestantCountRef.on("value",(data)=>{
-      //contestantCount = data.val();
-    //})
+    contestantCountRef.on("value",(data)=>{
+      contestantCount = data.val();
+    })
 
-    //contestantCountRef.on("value",()=>{
-      //contestantCount = data.val();
-    //})
+    
 
-    //contestantCountRef.on("value",(data)=>{
-      //contestantCount = data();
-    //})
+    
   }
 
   updateCount(count){
-    //database.ref("/").update({
-      //contestantCount: count
-    //});
+    database.ref("/").update({
+      contestantCount: count
+    });
 
-    //database.ref("/").update({
-      //contestantCount: 0
-    //});
 
-    //database.ref("/").update({
-      //contestantCount: counts
-    //});
 
-    //database("/").update({
-      //contestantCount = count
-    //});
+    
   }
 
  
